abee389cf3a4c06a70d52d80b8aa40d8d66f95b4
aim467 <709544711@qq.com>
2021-06-04 18:31:13 +0800

    first commit

---
+++ src/main/java/com/root2z/MyPersonalBlogApplication.java
@@ -0,0 +1,13 @@
+package com.root2z;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyPersonalBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyPersonalBlogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/root2z/MyPersonalBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.root2z;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyPersonalBlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
