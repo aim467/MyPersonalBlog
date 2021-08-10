49fe7dbbe754681e864a9c82b1f30bcb1e73398e
aim467 <709544711@qq.com>
2021-08-10 17:55:43 +0800

    编写READMe.md，完工

--- README.md
+++ README.md
@@ -1,3 +1,47 @@
-# 个人博客
+# 关于项目
 
-基于 SpringBoot 的个人博客
+该项目是基于 `SpringBoot` + `Mybatis` 实现的个人博客系统。
+
+主要的使用的技术和软件如下：
+
+* SpringBoot
+* Mybatis
+* Maven
+* MySQL
+* Idea
+
+该项目的主要特性如下：
+
+* 使用阿里云OSS管理静态资源
+* 在编写文章时实现粘贴图片上传到阿里云OSS
+* 后台管理提供多种样式选择
+* ...
+
+对于一个个人博客来说，这个博客系统还是有很多不完美的地方，只是达到了一个最基础最基础的博客系统该有的功能，在未来的话，我会持续为这个博客进行优化，争取做的更好。
+
+## 效果预览
+
+首页
+
+![](docs/index.png)
+
+分类页
+
+![](docs/category.png)
+
+文章详情页
+
+![](docs/detail.png)
+
+后台登录页
+
+![](docs/login.png)
+
+文章管理页
+
+![](docs/backadmin.png)
+
+## 如何使用
+
+请在 `application.dev.yml` 中配置数据库信息，数据库脚本已经导入 `docs` 文件夹，在 `admin` 表内创建一个帐号，记得密码使用 `MD5Utils` 类加密；最后在 `application.yml`
+中配置阿里云OSS配置属性，然后就可以运行了。
--- docs/schema.sql
+++ docs/schema.sql
@@ -1,116 +1,112 @@
--- MYBLOG.admin definition
-
-CREATE TABLE `admin` (
-                         `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '管理员主键ID',
-                         `nickname` varchar(20) DEFAULT NULL COMMENT '管理员昵称',
-                         `avatar` varchar(255) DEFAULT NULL COMMENT '头像',
-                         `username` varchar(255) DEFAULT NULL COMMENT '用户名',
-                         `password` varchar(255) DEFAULT NULL COMMENT '密码',
-                         `create_time` datetime DEFAULT NULL COMMENT '创建时间',
-                         `update_time` datetime DEFAULT NULL COMMENT '修改时间',
-                         `status` int(11) DEFAULT NULL COMMENT '用户状态, 1:启用,0:禁用',
-                         PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-
--- MYBLOG.article definition
-
-CREATE TABLE `article` (
-                           `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '文章的主键ID',
-                           `title` varchar(255) DEFAULT NULL COMMENT '文章标题',
-                           `introduce` varchar(500) DEFAULT NULL COMMENT '文章介绍，引言',
-                           `content` longtext DEFAULT NULL COMMENT '正文',
-                           `author` varchar(255) DEFAULT NULL COMMENT '作者名字',
-                           `publish_time` datetime DEFAULT NULL COMMENT '发布时间',
-                           `update_time` datetime DEFAULT NULL COMMENT '更新时间',
-                           `status` int(11) DEFAULT NULL COMMENT '文章状态。1:发布,0:草稿',
-                           PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-
--- MYBLOG.article_category definition
-
-CREATE TABLE `article_category` (
-                                    `article_id` int(11) NOT NULL DEFAULT 0 COMMENT '文章ID',
-                                    `category_id` int(11) NOT NULL DEFAULT 0 COMMENT '分类ID',
-                                    PRIMARY KEY (`article_id`,`category_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-
--- MYBLOG.article_tag definition
-
-CREATE TABLE `article_tag` (
-                               `article_id` int(11) NOT NULL DEFAULT 0 COMMENT '文章ID',
-                               `tag_id` int(11) NOT NULL DEFAULT 0 COMMENT '标签ID',
-                               PRIMARY KEY (`article_id`,`tag_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-
--- MYBLOG.category definition
-
-CREATE TABLE `category` (
-                            `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '分类主键',
-                            `name` varchar(255) DEFAULT NULL COMMENT '分类名字',
-                            PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-
--- MYBLOG.comment definition
-
-CREATE TABLE `comment` (
-                           `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '评论主键ID',
-                           `commentator` varchar(255) DEFAULT NULL COMMENT '评论者',
-                           `avatar` varchar(255) DEFAULT NULL COMMENT '评论者头像',
-                           `content` varchar(255) DEFAULT NULL COMMENT '评论内容',
-                           `article_id` int(11) DEFAULT NULL COMMENT '文章ID',
-                           `parent_id` int(11) DEFAULT NULL COMMENT '评论父ID',
-                           PRIMARY KEY (`id`),
-                           KEY `article_id` (`article_id`),
-                           CONSTRAINT `article_comment` FOREIGN KEY (`article_id`) REFERENCES `article` (`id`) ON DELETE SET NULL ON UPDATE SET NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-
--- MYBLOG.friend definition
-
-CREATE TABLE `friend` (
-                          `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '友链主键',
-                          `name` varchar(255) DEFAULT NULL COMMENT '名字',
-                          `url` varchar(255) DEFAULT NULL COMMENT '链接',
-                          `avatar` varchar(255) DEFAULT NULL COMMENT '友链头像',
-                          `description` varchar(255) DEFAULT NULL COMMENT '友链描述',
-                          PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-
--- MYBLOG.message definition
-
-CREATE TABLE `message` (
-                           `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '留言主键ID',
-                           `author` varchar(255) DEFAULT NULL COMMENT '留言者名字',
-                           `avatar` varchar(255) DEFAULT NULL COMMENT '留言者头像',
-                           `url` varchar(255) DEFAULT NULL COMMENT '留言者URL',
-                           `content` varchar(255) DEFAULT NULL COMMENT '留言内容',
-                           `create_time` datetime DEFAULT NULL COMMENT '留言时间',
-                           `parent_id` int(11) DEFAULT NULL COMMENT '父留言ID',
-                           PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-
--- MYBLOG.tag definition
-
-CREATE TABLE `tag` (
-                       `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '标签主键ID',
-                       `name` varchar(255) DEFAULT NULL COMMENT '标签名字',
-                       PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- MYBLOG.log definition
-
-create table `log`
+create table admin
 (
-    `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '日志ID',
-    `username` VARCHAR(255) DEFAULT NULL COMMENT '登录用户',
-    `ipaddress` VARCHAR(255) DEFAULT NULL COMMENT '登录IP',
-    `login_time` datetime DEFAULT NULL COMMENT '登录时间',
-    PRIMARY KEY (`id`)
-)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+    id          int auto_increment comment '管理员主键ID'
+        primary key,
+    nickname    varchar(20) null comment '管理员昵称',
+    avatar      varchar(255) null comment '头像',
+    username    varchar(255) null comment '用户名',
+    password    varchar(255) null comment '密码',
+    email       varchar(255) null,
+    intro       varchar(255) null,
+    create_time datetime null comment '创建时间',
+    update_time datetime null comment '修改时间',
+    status      int null comment '用户状态, 1:启用,0:禁用'
+) charset=utf8;
+
+create table article_tag
+(
+    article_id int default 0 not null comment '文章ID',
+    tag_id     int default 0 not null comment '标签ID',
+    primary key (article_id, tag_id)
+) charset=utf8;
+
+create table category
+(
+    id   int auto_increment comment '分类主键'
+        primary key,
+    name varchar(255) null comment '分类名字'
+) charset=utf8;
+
+create table article
+(
+    id           int auto_increment comment '文章的主键ID'
+        primary key,
+    title        varchar(255) null comment '文章标题',
+    cover        varchar(255) null comment '文章封面图',
+    introduce    varchar(500) null comment '文章介绍，引言',
+    content      longtext null comment '正文',
+    author       varchar(255) null comment '作者名字',
+    publish_time datetime null comment '发布时间',
+    update_time  datetime null comment '更新时间',
+    status       int null comment '文章状态。1:发布,0:草稿',
+    is_top       int default 0 null comment '是否置顶文章，1为是，0为否',
+    is_recommend int default 0 null comment '是否为推荐文章，1为是，0为否',
+    category_id  int null,
+    constraint articleCategory
+        foreign key (category_id) references category (id)
+            on update set null on delete set null
+) charset=utf8;
+
+create table comment
+(
+    id          int auto_increment comment '评论主键ID'
+        primary key,
+    commentator varchar(255) null comment '评论者',
+    avatar      varchar(255) null comment '评论者头像',
+    content     varchar(255) null comment '评论内容',
+    is_active   int(2) default 1 null comment '删除状态位',
+    email       varchar(255) null comment '邮箱',
+    create_time datetime default current_timestamp() null comment '创建时间',
+    article_id  int null comment '文章ID',
+    parent_id   int null comment '评论父ID',
+    constraint article_comment
+        foreign key (article_id) references article (id)
+            on update set null on delete set null
+) charset=utf8;
+
+create
+index article_id
+	on comment (article_id);
+
+create table friend
+(
+    id          int auto_increment comment '友链主键'
+        primary key,
+    name        varchar(255) null comment '名字',
+    url         varchar(255) null comment '链接',
+    avatar      varchar(255) null comment '友链头像',
+    description varchar(255) null comment '友链描述'
+) charset=utf8;
+
+create table log
+(
+    id         int auto_increment comment '日志ID'
+        primary key,
+    username   varchar(255) null comment '登录用户',
+    ipaddress  varchar(255) null comment '登录IP',
+    login_time datetime null comment '登录时间'
+) charset=utf8;
+
+create table message
+(
+    id          int auto_increment comment '留言主键ID'
+        primary key,
+    author      varchar(255) null comment '留言者名字',
+    email       varchar(255) null comment '留言人邮箱',
+    avatar      varchar(255) null comment '留言者头像',
+    url         varchar(255) null comment '留言者URL',
+    content     varchar(255) null comment '留言内容',
+    create_time datetime default current_timestamp() null comment '留言时间',
+    is_active   int(2) default 1 null,
+    parent_id   int null comment '父留言ID'
+) charset=utf8;
+
+create table tag
+(
+    id   int auto_increment comment '标签主键ID'
+        primary key,
+    name varchar(255) null comment '标签名字',
+    constraint name
+        unique (name)
+) charset=utf8;
+
